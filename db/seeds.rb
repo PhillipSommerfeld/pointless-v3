a41c3d52a631143f6e36444ca40a6cf24d5489ca
Phillip Sommerfeld <sommerfeldphillip@gmail.com>
2021-11-22 16:17:12 +0000

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Offer.delete_all
+User.delete_all
+
+10.times do
+  users = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    address: Faker::Address.street_address
+  )
+  users.save
+end
+
+User.all.each do |user|
+  2.times do
+    offers = Offer.create(
+      item_name: Faker::Movie.title,
+      price: rand(5..20),
+      description: Faker::Lorem.paragraph,
+      image_url: "https://picsum.photos/#{rand(100..300)}",
+      quantity: rand(1..5),
+      user_id: user.id
+    )
+    offers.save
+  end
+end
