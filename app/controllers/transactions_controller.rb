61bf4240ed7fb745ffae3004b442260da51ce651
Nuno Costa <nm.cdc@hotmail.com>
2021-11-22 17:30:55 +0000

    syntax error

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,5 @@
 class TransactionsController < ApplicationController
-    before_action :set_order, only: [:show, :edit, :update, :destroy]
+    before_action :set_transaction, only: [:show, :edit, :update, :destroy]
 
 #Basic Crud Actions
   def index
