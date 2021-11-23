ad7e6ea7eb0d5988721a52fa65fac7882766e96d
Nuno Costa <nm.cdc@hotmail.com>
2021-11-23 20:45:53 +0000

    fixed it back, no change

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+devise_for :users
   root to: 'pages#home'
   resources :offers
-  resources :transfers
   get "dashboard", to: "pages#dashboard"
-  resources :offers, only: [:show]
-  resources :transactions, only: [:new, :create, :index] do
+  resources :offers, only: [:show] do
+    resources :transactions, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post "/offers" => "transfers#offers"
-  post "new" => 'transfers#new'
 end
