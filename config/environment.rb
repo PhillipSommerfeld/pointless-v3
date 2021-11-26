96a74da5bebf21bd540da8dedbee7a757a7ebd1d
Alex Paul <abpaul87@gmail.com>
2021-11-26 11:41:36 +0100

    WIP - adjusting show page deisng. Don't merge

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+CLOUDINARY_URL = 'cloudinary://627632453244442:_p6dVOLU9viphVqVAOwTyXqfRxM@jansommer'
